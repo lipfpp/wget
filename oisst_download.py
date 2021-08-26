16716ed3aa21b4b99d15b24658f50e0df5b04dff
FELIPE FURTADO PINHO <85547354+lipfpp@users.noreply.github.com>
2021-08-26 08:36:24 -0300

    Update oisst_download.py

--- oisst_download.py
+++ oisst_download.py
@@ -8,4 +8,4 @@ for i in range(start_year, end_year+1):
      oisst_data.write('ftp://ftp2.psl.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.' + str(i) + '.nc\n')
 oisst_data.close()
 
-!wget -i /..../list_oisst.txt
+!wget -i /..../list_oisst.txt -P /..../
